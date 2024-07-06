82bfac775d25366ca933bbc0f0ca731bb38b4f26
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-07-06 12:50:26 +0530

    update

--- furni/src/component/Home.jsx
+++ furni/src/component/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../style/home.css'
+import '../style/shopcom.css'
+
 import CarouselComponent from '../component/CarouselComponent '
 
 
@@ -37,19 +39,22 @@ function Home() {
             <div className="chair_one">
               <img src="https://themewagon.github.io/furni/images/product-1.png" alt="img" />
               <p>Nordic Chair</p>
-              <span>$50.00</span>
+              <span>$50.00</span> <br />
+              <button className='addtocard'>Add</button>
             </div>
 
             <div className="chair_two">
             <img src="https://themewagon.github.io/furni/images/product-2.png" alt="img" />
               <p>Kruzo Aero Chair</p>
-              <span>$78.00</span>
+              <span>$78.00</span> <br />
+              <button className='addtocard'>Add</button>
             </div>
 
             <div className="chair_three">
             <img src="https://themewagon.github.io/furni/images/product-3.png" alt="img" />
               <p>Ergonomic Chair</p>
-              <span>$43.00</span>
+              <span>$43.00</span> <br />
+              <button className='addtocard'>Add</button>
             </div>
 
           </div>
--- furni/src/component/Shop.jsx
+++ furni/src/component/Shop.jsx
@@ -1,9 +1,101 @@
 import React from 'react'
+import '../style/home.css'
+import '../style/shopcom.css'
+
 
 function Shop() {
   return (
-    <div>
+    <div className='shopcom_main'>
+      <div className='shopcom'>
         <h1>Shop</h1>
+      </div>
+
+
+      <div className="Crafted-two">
+            <div className="chair_one">
+              <img src="https://themewagon.github.io/furni/images/product-1.png" alt="img" />
+              <p>Nordic Chair</p>
+              <span>$50.00</span> <br />
+              <button className='addtocard'>Add </button>
+            </div>
+
+            <div className="chair_two">
+            <img src="https://themewagon.github.io/furni/images/product-2.png" alt="img" />
+              <p>Kruzo Aero Chair</p>
+              <span>$78.00</span> <br />
+              <button className='addtocard'>Add </button>
+            </div>
+
+            <div className="chair_three">
+            <img src="https://themewagon.github.io/furni/images/product-3.png" alt="img" />
+              <p>Ergonomic Chair</p>
+              <span>$43.00</span> <br />
+              <button className='addtocard'>Add </button>
+            </div>
+            <div className="chair_one">
+              <img src="https://themewagon.github.io/furni/images/product-1.png" alt="img" />
+              <p>Nordic Chair</p>
+              <span>$50.00</span> <br />
+              <button className='addtocard'>Add </button>
+            </div>
+
+            <div className="chair_one">
+              <img src="https://themewagon.github.io/furni/images/product-1.png" alt="img" />
+              <p>Nordic Chair</p>
+              <span>$50.00</span> <br />
+              <button className='addtocard'>Add </button>
+            </div>
+
+            <div className="chair_two">
+            <img src="https://themewagon.github.io/furni/images/product-2.png" alt="img" />
+              <p>Kruzo Aero Chair</p>
+              <span>$78.00</span> <br />
+              <button className='addtocard'>Add </button>
+            </div>
+
+            <div className="chair_three">
+            <img src="https://themewagon.github.io/furni/images/product-3.png" alt="img" />
+              <p>Ergonomic Chair</p>
+              <span>$43.00</span> <br />
+              <button className='addtocard'>Add </button>
+            </div>
+            <div className="chair_one">
+              <img src="https://themewagon.github.io/furni/images/product-1.png" alt="img" />
+              <p>Nordic Chair</p>
+              <span>$50.00</span> <br />
+              <button className='addtocard'>Add </button>
+            </div>
+
+            
+            <div className="chair_one">
+              <img src="https://themewagon.github.io/furni/images/product-1.png" alt="img" />
+              <p>Nordic Chair</p>
+              <span>$50.00</span> <br />
+              <button className='addtocard'>Add </button>
+            </div>
+
+            <div className="chair_two">
+            <img src="https://themewagon.github.io/furni/images/product-2.png" alt="img" />
+              <p>Kruzo Aero Chair</p>
+              <span>$78.00</span> <br />
+              <button className='addtocard'>Add </button>
+            </div>
+
+            <div className="chair_three">
+            <img src="https://themewagon.github.io/furni/images/product-3.png" alt="img" />
+              <p>Ergonomic Chair</p>
+              <span>$43.00</span> <br />
+              <button className='addtocard'>Add </button>
+            </div>
+            <div className="chair_one">
+              <img src="https://themewagon.github.io/furni/images/product-1.png" alt="img" />
+              <p>Nordic Chair</p>
+              <span>$50.00</span> <br />
+              <button className='addtocard'>Add </button>
+            </div>
+
+          </div>
+
     </div>
   )
 }
